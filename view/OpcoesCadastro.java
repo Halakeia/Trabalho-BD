270f74525e92a095a68fe783ee4c9a323a1f0a4a
GuilhermeMiranda553 <guilherme.vmoliveira@hotmail.com>
2025-02-05 05:13:18 -0300

    - Interação com Cadastro de Cliente Completo
    (Inserir, Buscar, Editar, Excluir)

--- view/OpcoesCadastro.java
+++ view/OpcoesCadastro.java
@@ -8,7 +8,7 @@ import dao.ClienteDAO;
 import model.cliente;
 
 public class OpcoesCadastro {
-    ClienteDAO cliente = new ClienteDAO();
+    ClienteDAO clienteDAO = new ClienteDAO();
     Scanner scanner = new Scanner(System.in);
     public void cadastroCliente(int usuarioID) {
         int opcao;
@@ -40,36 +40,80 @@ public class OpcoesCadastro {
 
                     LocalDate data = LocalDate.parse(dataStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
-                    cliente novoCliente = new cliente(nome, cpf, email, data);
-                    cliente.inserirCliente(novoCliente);
+                    cliente cliente = new cliente(nome, cpf, email, data);
+                    clienteDAO.inserirCliente(cliente);
                 break;
 
                 case 2:
                     //Buscar dados
-                    cliente busca;
                     scanner.nextLine();
                     System.out.print("\n-----------------------\n");
                     System.out.println("Buscar:");
                     System.out.print("Digite o cpf do cliente: ");
                     cpf = scanner.nextLine();
-                    System.out.println("CPF: " + cpf);
-                    if ((busca = cliente.buscarCliente(cpf)) == null) {
+
+                    if ((cliente = clienteDAO.buscarCliente(cpf)) == null) {
                         System.out.println("\nCliente não encontrado!\n");
                         break;
                     }
                     System.out.println("\nResultado da busca pelo cliente:");
-                    System.out.println("Nome: " + busca.getNome());
-                    System.out.println("CPF: " + busca.getCpf());
-                    System.out.println("Email: " + busca.getEmail());
-                    System.out.println("Data de Nascimento: " + busca.getDataNascimento());
+                    System.out.println("Nome: " + cliente.getNome());
+                    System.out.println("CPF: " + cliente.getCpf());
+                    System.out.println("Email: " + cliente.getEmail());
+                    System.out.println("Data de Nascimento: " + cliente.getDataNascimento());
                 break;
 
                 case 3:
                     //Editar dados
+                    scanner.nextLine();
+                    System.out.print("\n-----------------------\n");
+                    System.out.println("Alterar:");
+                    System.out.print("Digite o cpf do cliente: ");
+                    cpf = scanner.nextLine();
+
+                    if ((cliente = clienteDAO.buscarCliente(cpf)) == null) {
+                        System.out.println("\nCliente não encontrado!\n");
+                        break;
+                    }
+
+                    System.out.print("Digite o nome: ");
+                    nome =(scanner.nextLine());
+                    System.out.print("Digite o email: ");
+                    email = (scanner.nextLine());
+                    System.out.print("Digite a data de Nascimento: ");
+                    dataStr = scanner.nextLine();
+
+                    data = LocalDate.parse(dataStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                    
+                    cliente = new cliente(nome, cpf, email, data);
+                    
+                    clienteDAO.atualizarCliente(cliente);
+
                 break; 
 
                 case 4:
                     //Excluir dados
+                    scanner.nextLine();
+                    System.out.print("\n-----------------------\n");
+                    System.out.println("Excluir:");
+                    System.out.print("Digite o cpf do cliente: ");
+                    cpf = scanner.nextLine();
+
+                    if ((cliente = clienteDAO.buscarCliente(cpf)) == null) {
+                        System.out.println("\nCliente não encontrado!\n");
+                        break;
+                    }
+
+                    System.out.print("\nDeseja excluir o cadastro de " + cliente.getNome() + "? (s - confirmar): ");
+                    String confirm = scanner.nextLine();
+                    System.out.println("Excluir:" + confirm);
+                    if ((confirm.equals("s")) || (confirm.equals("S"))) {
+                        clienteDAO.deletarCliente(cpf);
+                        break;
+                    } 
+
+                    System.out.println("\nOpção inválida!");
+
                 break; 
 
                 case 5:
